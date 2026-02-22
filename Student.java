22260a6d41617dcde014532ea1588f3fceacce13
aron <brillantesaron37@gmail.com>
2026-02-22 15:02:51 +0800

    Implemented Student Class
    Added a new field in the Student class to store additional student information.

--- Student.java
+++ Student.java
@@ -1,11 +1,30 @@
 public class Student {
+    private int studID;
+    private String firstName;
+    private String lastName;
+    private int age;
     private int contactNumber;
     private String address;
 
-    public Student(int contactNumber, String address) {
-
+    public Student(int studID, String firstName, String lastName, int age, int contactNumber, String address) {
+        this.studID = studID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
         this.contactNumber = contactNumber;
         this.address = address;
+    }
+    public int getStudID() { 
+        return studID; 
+    }
+    public String getFirstName() { 
+        return firstName; 
+    }
+    public String getLastName() { 
+        return lastName; 
+    }
+    public int getAge() { 
+        return age; 
     }
     public int getContactNumber() { 
         return contactNumber; 
